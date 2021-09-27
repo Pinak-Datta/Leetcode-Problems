cf1739c82a736e454efeb9095b4fe1c45f530365
Pinak Datta <87294070+Pinak-Datta@users.noreply.github.com>
2021-09-27 09:54:53 +0530

    Create Create_Target_array_in_order.java

---
+++ Arrays/Create_Target_array_in_order.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int[] createTargetArray(int[] nums, int[] index) {
+        List<Integer> lst = new ArrayList<>(); // use list to add elements 
+        for (int i = 0; i < index.length; i++) lst.add(index[i], nums[i]);
+        
+        int[] res = new int[nums.length]; // convert list to array
+        for (int i = 0; i < index.length; i++) res[i] = lst.get(i);
+        return res;
+    }
+}
